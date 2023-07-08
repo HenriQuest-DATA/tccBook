177f658c5cd555623b1f1f1c933020aeb5e2e950
Henrique Aquino <henrique.aquino-extern@renault.com>
2023-07-08 15:08:55 -0300

    v1

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  productio: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDVygE9QKFGbfbaMGFXWK-KHaVjHs3zBPE",
+    authDomain: "tccbook-29369.firebaseapp.com",
+    projectId: "tccbook-29369",
+    storageBucket: "tccbook-29369.appspot.com",
+    messagingSenderId: "774568612403",
+    appId: "1:774568612403:web:5b4a4d25fb59aca3498946",
+    measurementId: "G-TWH8PFLKQ8"
+  }
+};
